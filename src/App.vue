76e090fcca6ffdaa5b30f6fa0130d4e456dec5f6
iman <imanimen81@gmail.com>
2024-09-13 16:07:32 +0330

    initial vue composition api

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">user {{ name }} is active</p>
+  <p v-else-if="status === 'pending'">user {{ name }} is pending</p>
+  <p v-else>user {{ name }} is not active</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Submit</button>
+  </form>
+
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, tasks) in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a :href="link">Click for Google</a>
+
+  <br>
+  <button @click="toggleStatus">Change Status</button>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+  const name = ref("John Doe")
+  const status = ref('active')
+  const tasks = ref(['task-1', 'task-2', 'task-3'])
+  const newTask = ref('')
+
+  const toggleStatus = () => {
+    if (status.value === 'active') {
+      status.value = 'pending'
+    } else if (status.value === 'pending') {
+      status.value = 'inactive'
+    } else {
+      status.value = 'active'
+    }
+  };
+
+  const addTask = () => {
+    if (newTask.value.trim() !== "") {
+      tasks.value.push(newTask.value);
+      newTask.value = ""
+    }
+  }
+</script>
---
+++ src/App2.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">user {{ name }} is active</p>
+  <p v-else-if="status === 'pending'">user {{ name }} is pending</p>
+  <p v-else>user {{ name }} is not active</p>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, tasks) in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a :href="link">Click for Google</a>
+
+  <br>
+  <button @click="toggleStatus">Change Status</button>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "John Doe",
+      status: "pending",
+      tasks: ['task-1', 'task-2', 'task-3'],
+      link: 'https://google.com'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending'
+      } else if (this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'active'
+      }
+    }
+  }
+};
+</script>
