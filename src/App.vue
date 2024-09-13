cde4d5514a9d55f91f8aa10706629d3793017074
iman <imanimen81@gmail.com>
2024-09-13 20:07:28 +0330

    feat: job listing, job listing single, jobs.json

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   <Navbar/>
   <Hero title="Test Title" subTitle="Become a Vue Dev"/>
   <HomeCards/>
+  <JobList/>
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
 import HomeCards from './components/HomeCards.vue';
-
+import JobList from './components/JobList.vue';
 </script>
