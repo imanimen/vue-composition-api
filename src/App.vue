a7605e8aec3206c85bba652d2c2acea6308e9816
iman <imanimen81@gmail.com>
2024-09-13 17:15:34 +0330

    feat: Hero cmp props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <Navbar/>
+  <Hero title="Test Title" subTitle="Become a Vue Dev"/>
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue';
-
+import Hero from './components/Hero.vue';
 
 </script>
