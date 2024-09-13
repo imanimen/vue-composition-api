e1ccafc1e6420b90ea8a3674f2dde7b5e5877a6f
iman <imanimen81@gmail.com>
2024-09-13 17:30:41 +0330

    feat: add <HomeCard/> cmp

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Navbar/>
   <Hero title="Test Title" subTitle="Become a Vue Dev"/>
+  <HomeCards/>
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
 
 </script>
