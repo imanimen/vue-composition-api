f620dee71b14253324ea4556db91b9fe94ccad2b
iman <imanimen81@gmail.com>
2024-09-13 20:10:53 +0330

    fix: job routing

--- src/components/JobListingSingle.vue
+++ src/components/JobListingSingle.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { defineProps } from 'vue';
 
-
 defineProps({
     job: Object 
 });
@@ -30,7 +29,7 @@ defineProps({
               {{ job.location }}
             </div>
             <a
-              href="job.html"
+              :href="'/job/' + job.id"
               class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
             >
               Read More
